a6fdeef2554eaf07eb97046086a4c24f5bd0f43c
AhmadKlfr <Ahmad.khalilfar@gmail.com>
2019-05-26 12:00:16 +0430

    add errors table

--- server/server.js
+++ server/server.js
@@ -91,9 +91,133 @@ boot(app, __dirname, function(err) {
       });
 
       socket.on('data', function (value) {
-        console.log('data: ', value);
-        app.socketServer.emit('check', "success");
+        var parts = value.split(',');
+        var errorNames = [];
+
+        parts.forEach((part, index) => {
+          for (var i = (part.length - 1); i >= 0; i--) {
+            
+            if(part.charAt(i) === "1") {
+              errorNames.push(getCodeName(index, (part.length - i)));
+            }
+          }
+        });
+
+        console.log('data: ', errorNames);
+        app.socketServer.emit('check', errorNames.join(' - '));
       });
+
+      function getCodeName(index, i) {
+        switch (index) {
+          case 0:
+            switch (i) {
+              case 1:
+                return 'FTO';
+              case 2:
+                return 'FIR';
+              case 3:
+                return 'Break';
+              case 4:
+                return '110';
+              case 5:
+                return '71';
+              case 6:
+                return '66';
+              case 7:
+                return '69';  
+              case 8:
+                return '68';
+              default:
+                break;
+            }
+            break;
+          
+          case 1:
+            switch (i) {
+              case 1:
+                return '1CF';
+              case 2:
+                return 'CF3';
+              case 3:
+                return 'CA1';
+              case 4:
+                return 'CAN';
+              case 5:
+                return 'EC1';
+              case 6:
+                return 'ECN';
+              default:
+                break;
+            }
+            break;
+
+          case 2:
+            switch (i) {
+              case 1:
+                return 'R input';
+              case 2:
+                return 'S input';
+              case 3:
+                return 'T input';
+              case 4:
+                return 'U1 output';
+              case 5:
+                return 'V1 output';
+              case 6:
+                return 'W1 output';
+              case 7:
+                return 'U2 output';
+              case 8:
+                return 'V2 output';
+              default:
+                break;
+            }
+            break;
+
+          case 3:
+            switch (i) {
+              case 1:
+                return 'W2 output';
+              case 2:
+                return 'Door error';
+              case 3:
+                return 'contact error';
+              case 4:
+                return 'Emergency';
+              case 5:
+                return 'cant calibration';
+              case 6:
+                return 'jump door';
+              case 8:
+                return 'control phase';
+              default:
+                break;
+            }
+            break;
+
+          case 4:
+            switch (i) {
+              case 1:
+                return 'CA1 larzesh';
+              case 2:
+                return 'CAN larzesh';
+              case 3:
+                return 'EC1 larzesh';
+              case 4:
+                return 'ECN larzesh';
+              case 5:
+                return 'CF1 larzesh';
+              case 6:
+                return 'CF3 larzesh';
+              default:
+                break;
+            }
+            break;
+        
+          default:
+            break;
+        }
+      }
     });
   }
 });
